6b18a4e6a5c4e10b4b683b7337a7a5bd113eee47
Adriano Schmidt <adrvschmidt@gmail.com>
2023-01-29 18:22:00 -0300

    Added animation when scroll a list

--- app/src/main/java/br/com/schmidt/mealsapp/ui/details/MealDetailsScreen.kt
+++ app/src/main/java/br/com/schmidt/mealsapp/ui/details/MealDetailsScreen.kt
@@ -2,13 +2,17 @@ package br.com.schmidt.mealsapp.ui.details
 
 import androidx.compose.animation.animateColor
 import androidx.compose.animation.core.animateDp
+import androidx.compose.animation.core.animateDpAsState
 import androidx.compose.animation.core.updateTransition
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.Button
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Card
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
@@ -16,57 +20,61 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.max
 import br.com.schmidt.mealsapp.model.response.MealResponse
 import coil.compose.rememberImagePainter
 import coil.transform.CircleCropTransformation
+import kotlin.math.max
+import kotlin.math.min
 
 @Composable
 fun MealDetailsScreen(meal: MealResponse?) {
-    var profilePictureState by remember {
-        mutableStateOf(ProfilePictureState.Normal)
-    }
-    val transition = updateTransition(targetState = profilePictureState, label = "")
-    val imageSizeDp by transition.animateDp(targetValueByState = { it.size }, label = "")
-    val borderWidth by transition.animateDp(targetValueByState = { it.borderWidth }, label = "")
-    val color by transition.animateColor(targetValueByState = { it.color }, label = "")
 
-    Box {
+    val scrollState = rememberScrollState()
+    val offset = min(1f, 1 - (scrollState.value / 600f))
+    val size by animateDpAsState(targetValue = max(100.dp, 200.dp * offset))
+
+    Surface(color = MaterialTheme.colors.background) {
         Column {
-            Row {
-                Card(
-                    modifier = Modifier.padding(16.dp),
-                    shape = CircleShape,
-                    border = BorderStroke(width = borderWidth, color = color)
-                ) {
-                    Image(
-                        painter = rememberImagePainter(data = meal?.imageUrl ?: "", builder = {
-                            transformations(CircleCropTransformation())
-                        }),
-                        contentDescription = null,
+            Surface(elevation = 4.dp) {
+                Row(modifier =  Modifier.fillMaxWidth()) {
+                    Card(
+                        modifier = Modifier.padding(16.dp),
+                        shape = CircleShape,
+                        border = BorderStroke(width = 2.dp, color = Color.Green)
+                    ) {
+                        Image(
+                            painter = rememberImagePainter(data = meal?.imageUrl ?: "", builder = {
+                                transformations(CircleCropTransformation())
+                            }),
+                            contentDescription = null,
+                            modifier = Modifier
+                                .size(size)
+                        )
+                    }
+                    Text(
+                        text = meal?.name ?: "Nulo",
                         modifier = Modifier
-                            .size(imageSizeDp)
-                            .padding(8.dp)
+                            .padding(16.dp)
+                            .align(Alignment.CenterVertically)
                     )
                 }
-                Text(
-                    text = meal?.name ?: "Nulo",
-                    modifier = Modifier
-                        .padding(16.dp)
-                        .align(Alignment.CenterVertically)
-                )
             }
-            Button(onClick = {
-                profilePictureState =
-                    if (profilePictureState == ProfilePictureState.Normal) ProfilePictureState.Expanded else ProfilePictureState.Normal
-            }, modifier = Modifier.padding(16.dp)) {
-                Text(text = "Change state of meal profile picture")
-
+            Column(modifier = Modifier.verticalScroll(scrollState)) {
+                Text(text = "This is a text element", modifier = Modifier.padding(32.dp))
+                Text(text = "This is a text element", modifier = Modifier.padding(32.dp))
+                Text(text = "This is a text element", modifier = Modifier.padding(32.dp))
+                Text(text = "This is a text element", modifier = Modifier.padding(32.dp))
+                Text(text = "This is a text element", modifier = Modifier.padding(32.dp))
+                Text(text = "This is a text element", modifier = Modifier.padding(32.dp))
+                Text(text = "This is a text element", modifier = Modifier.padding(32.dp))
+                Text(text = "This is a text element", modifier = Modifier.padding(32.dp))
+                Text(text = "This is a text element", modifier = Modifier.padding(32.dp))
+                Text(text = "This is a text element", modifier = Modifier.padding(32.dp))
+                Text(text = "This is a text element", modifier = Modifier.padding(32.dp))
+                Text(text = "This is a text element", modifier = Modifier.padding(32.dp))
+                Text(text = "This is a text element", modifier = Modifier.padding(32.dp))
             }
         }
     }
-}
-
-enum class ProfilePictureState(val color: Color, val size: Dp, val borderWidth: Dp) {
-    Normal(Color.Magenta, 120.dp, 8.dp),
-    Expanded(Color.Green, 200.dp, 24.dp)
 }
