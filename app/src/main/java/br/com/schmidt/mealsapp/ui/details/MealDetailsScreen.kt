5d245bd733e1c4a5211a023919b739dc1fd75eb9
Adriano Schmidt <adrvschmidt@gmail.com>
2023-01-29 18:33:37 -0300

    Refactored identation

--- app/src/main/java/br/com/schmidt/mealsapp/ui/details/MealDetailsScreen.kt
+++ app/src/main/java/br/com/schmidt/mealsapp/ui/details/MealDetailsScreen.kt
@@ -1,43 +1,43 @@
 package br.com.schmidt.mealsapp.ui.details
 
-import androidx.compose.animation.animateColor
-import androidx.compose.animation.core.animateDp
 import androidx.compose.animation.core.animateDpAsState
-import androidx.compose.animation.core.updateTransition
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Card
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
-import androidx.compose.runtime.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.max
 import br.com.schmidt.mealsapp.model.response.MealResponse
 import coil.compose.rememberImagePainter
 import coil.transform.CircleCropTransformation
-import kotlin.math.max
 import kotlin.math.min
 
 @Composable
 fun MealDetailsScreen(meal: MealResponse?) {
 
-    val scrollState = rememberScrollState()
-    val offset = min(1f, 1 - (scrollState.value / 600f))
+    val scrollState = rememberLazyListState()
+    val offset = min(
+        1f,
+        1 - (scrollState.firstVisibleItemScrollOffset / 600f + scrollState.firstVisibleItemIndex)
+    )
     val size by animateDpAsState(targetValue = max(100.dp, 200.dp * offset))
 
     Surface(color = MaterialTheme.colors.background) {
         Column {
             Surface(elevation = 4.dp) {
-                Row(modifier =  Modifier.fillMaxWidth()) {
+                Row(modifier = Modifier.fillMaxWidth()) {
                     Card(
                         modifier = Modifier.padding(16.dp),
                         shape = CircleShape,
@@ -60,20 +60,15 @@ fun MealDetailsScreen(meal: MealResponse?) {
                     )
                 }
             }
-            Column(modifier = Modifier.verticalScroll(scrollState)) {
-                Text(text = "This is a text element", modifier = Modifier.padding(32.dp))
-                Text(text = "This is a text element", modifier = Modifier.padding(32.dp))
-                Text(text = "This is a text element", modifier = Modifier.padding(32.dp))
-                Text(text = "This is a text element", modifier = Modifier.padding(32.dp))
-                Text(text = "This is a text element", modifier = Modifier.padding(32.dp))
-                Text(text = "This is a text element", modifier = Modifier.padding(32.dp))
-                Text(text = "This is a text element", modifier = Modifier.padding(32.dp))
-                Text(text = "This is a text element", modifier = Modifier.padding(32.dp))
-                Text(text = "This is a text element", modifier = Modifier.padding(32.dp))
-                Text(text = "This is a text element", modifier = Modifier.padding(32.dp))
-                Text(text = "This is a text element", modifier = Modifier.padding(32.dp))
-                Text(text = "This is a text element", modifier = Modifier.padding(32.dp))
-                Text(text = "This is a text element", modifier = Modifier.padding(32.dp))
+            val dummyList = (0..100).map { it.toString() }
+            LazyColumn(state = scrollState) {
+                items(dummyList) { item ->
+                    Text(
+                        text = "Teste do LazyColumn $item", modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(8.dp)
+                    )
+                }
             }
         }
     }
